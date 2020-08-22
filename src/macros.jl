723bf7d66a9381f6cd21a947ecde21df798a12b9
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-08-22 13:46:38 +0200

    added some fun macros

---
+++ src/macros.jl
@@ -0,0 +1,133 @@
+using MacroTools
+
+function _get_varname(expr)
+    if MacroTools.@capture(expr, x_::T_)
+        x
+    else
+        expr
+    end
+end
+
+"""
+    @deftransform function transform(b::Bijector, x) ... end
+
+Essentially results in
+
+    function transform(b::Bijector, x) ... end
+    (b::Bijector)(x) = transform(b, x)
+"""
+macro deftransform(expr)
+    def = MacroTools.splitdef(expr)
+
+    # First arg should be the `Bijector`.
+    # Second arg should be the input.
+    bijector_def, input_def = def[:args]
+    body = def[:body]
+    name = esc(def[:name])
+
+    # extract the input variables
+    input_var = _get_varname(input_def)
+    bijector_var = _get_varname(bijector_def)
+
+    return quote
+        $(Base).@__doc__ function $(name)($bijector_def, $input_def)
+            $body
+        end
+
+        ($bijector_def)($input_def) = $(name)($bijector_var, $input_var)
+    end
+end
+
+
+### Sharing computation ###
+# function forward_clean(expr)
+#     if Meta.isexpr(expr, :macrocall) && length(expr.args) == 3
+#         if (expr.args[1] === Symbol("@transform")) || (expr.args[1] === Symbol("@logabsdetjac"))
+#             return expr.args[3]
+#         else
+#             return expr
+#         end
+#     else
+#         return expr
+#     end
+# end
+
+"""
+    @bijector function forward(b::Bijector, x) ... end
+
+Takes the method `forward` and uses it to define both `transform`
+and `logabsdetjac`, while ensuring that any shared computation is
+taken advantage of in `forward`.
+"""
+macro bijector(expr)
+    def = MacroTools.splitdef(expr)
+    body = def[:body]
+    args = def[:args]
+
+    # Figure out what is shared, and what isn't
+    shared_exprs = [] # beginning of `forward`
+    transform_exprs = []
+    logjac_exprs = []
+    tail_exprs = [] # goes at the end of `forward`
+
+    for expr in body.args
+        # # When using `@transform` and `@logabsdetjac`
+        # # Check if we're still sharing
+        # if Meta.isexpr(expr, :(=)) && Meta.isexpr(expr.args[2], :macrocall)
+        #     e = expr.args[2]
+        #     if length(e.args) == 3
+        #         if e.args[1] === Symbol("@transform")
+        #             push!(transform_exprs, Expr(:return, e.args[3]))
+        #             expr.args[2] = e.args[3]
+        #             push!(tail_exprs, expr)
+        #         elseif e.args[1] === Symbol("@logabsdetjac")
+        #             push!(logjac_exprs, Expr(:return, e.args[3]))
+        #             expr.args[2] = e.args[3]
+        #             push!(tail_exprs, expr)
+        #         end
+        #     end
+        # end
+
+        # When using `rv = ...` and `logabsdetjac = ...`
+        if Meta.isexpr(expr, :(=)) && expr.args[1] == :rv
+            push!(transform_exprs, Expr(:return, expr.args[2]))
+            push!(tail_exprs, expr)
+        elseif Meta.isexpr(expr, :(=)) && expr.args[1] == :logabsdetjac
+            push!(logjac_exprs, Expr(:return, expr.args[2]))
+            push!(tail_exprs, expr)
+        else
+            # If still sharing, add those expressions
+            issharing = (length(transform_exprs) == 0) && (length(logjac_exprs) == 0)
+            if issharing
+                push!(shared_exprs, expr)
+            else
+                push!(tail_exprs, expr)
+            end
+        end
+    end
+
+    # Add the shared computation
+    transform_full_exprs = copy(shared_exprs)
+    append!(transform_full_exprs, transform_exprs)
+
+    logjac_full_exprs = copy(shared_exprs)
+    append!(logjac_full_exprs, logjac_exprs)
+
+    # Remove the redundant macro's from the `forward` body
+    forward_full_exprs = vcat(shared_exprs, tail_exprs)
+
+    return quote
+        $(Bijectors).@deftransform function $(Bijectors).transform($(args...))
+            $(transform_full_exprs...)
+        end
+
+        function $(Bijectors).logabsdetjac($(args...))
+            $(logjac_full_exprs...)
+        end
+
+        function $(Bijectors).forward($(args...))
+            $(forward_full_exprs...)
+        end
+    end
+end
+
